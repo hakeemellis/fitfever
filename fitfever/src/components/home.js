45af3508f369892bf095a503060821a7dba2d0ba
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2023-08-29 12:32:07 -0400

    Updated

--- fitfever/src/components/home.js
+++ fitfever/src/components/home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="container mx-auto py-4">
           <div className="flex items-center justify-between">
             <div className="logo">
-              <h1 className="text-2xl font-bold">Fit Fever</h1>
+              <h1 className="text-2xl font-bold px-7">Fit Fever</h1>
             </div>
             <nav className="space-x-4">
               <a href="#" className="hover:text-gray-300">Home</a>
@@ -16,16 +16,16 @@ const Home = () => {
               <a href="#" className="hover:text-gray-300">About</a>
               <a href="#" className="hover:text-gray-300">Contact</a>
             </nav>
-            <div className="flex items-center space-x-4">
-              <a href="#" className="hover:text-gray-300">Cart</a>
-              <a href="#" className="hover:text-gray-300">Login</a>
+            <div className="flex items-center">
+              <a href="#" className="hover:text-gray-300 px-1">Cart</a>
+              <a href="#" className="hover:text-gray-300 px-8">Login</a>
             </div>
           </div>
         </div>
       </header>
 
       {/* Hero Banner */}
-      <section className="bg-gray-900 text-white h-screen flex items-center justify-center">
+      <section className="bg-gray-950 text-white h-screen flex items-center justify-center">
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">Welcome to Fit Fever</h1>
           <p className="text-lg">Your ultimate fitness destination</p>
