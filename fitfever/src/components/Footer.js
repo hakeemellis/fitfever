e953b18285b8292b8826e6d684e3b51aa277fcbc
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2023-09-05 19:35:10 -0400

    Updated

--- fitfever/src/components/Footer.js
+++ fitfever/src/components/Footer.js
@@ -7,20 +7,10 @@ const Footer = () => {
     <footer className="bg-black text-white py-4">
       <div className="container mx-auto text-center">
         <div className="flex justify-center space-x-4">
-          <a
-            href="https://www.linkedin.com/in/hakeem-ellis/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white hover:text-gray-300"
-          >
+          <a href="https://www.linkedin.com/in/hakeem-ellis/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
             <FontAwesomeIcon icon={faLinkedin} size="2x" />{/* style={{ color: '#FF5733' }} if color */}
           </a>
-          <a
-            href="https://github.com/hakeemellis"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white hover:text-gray-300"
-          >
+          <a href="https://github.com/hakeemellis" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
             <FontAwesomeIcon icon={faGithubSquare} size="2x" />
           </a>
         </div>
