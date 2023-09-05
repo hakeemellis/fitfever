11d01ba6433cefe1a41bf35287482d28e374792d
hakeemellis <114674803+hakeemellis@users.noreply.github.com>
2023-09-05 19:37:02 -0400

    Updated

--- fitfever/src/components/CartIcon.js
+++ fitfever/src/components/CartIcon.js
@@ -1,19 +1,8 @@
 import React from 'react';
 
 const CartIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={2}
-      d="M4 4h16a1 1 0 011 1v14a1 1 0 01-1 1H4a1 1 0 01-1-1V5a1 1 0 011-1zM2 6l2.81 9.5M22 6l-2.81 9.5"
-    />
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4h16a1 1 0 011 1v14a1 1 0 01-1 1H4a1 1 0 01-1-1V5a1 1 0 011-1zM2 6l2.81 9.5M22 6l-2.81 9.5"/>
   </svg>
 );
 
